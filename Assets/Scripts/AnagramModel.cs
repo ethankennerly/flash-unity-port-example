5ede139087c9252db6c64bbbf1f0a15934641f88
Ethan Kennerly <ethan@lumoslabs.com>
2015-11-08 15:37:05 -0800

    Stub word and level parameters variable.

--- Assets/Scripts/AnagramModel.cs
+++ Assets/Scripts/AnagramModel.cs
@@ -1,5 +1,12 @@
+using System.Collections.Generic;
+
 public class AnagramModel {
+	public List<string> word;
+
 	public void Start() {
+	}
+
+	private void trial(Dictionary<string, string> parameters) {
 	}
 
 	public void Update(float deltaSeconds) {
